767211abaef3285204af9ce8b2a9ac003747a879
luizlobo <andrinolobo@gmail.com>
2026-01-19 18:44:19 -0300

    Adiciona projeto completo

---
+++ src/test/java/br/com/luiz/rest_with_spring_boot_and_java_luiz/RestWithSpringBootAndJavaLuizApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luiz.rest_with_spring_boot_and_java_luiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaLuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
